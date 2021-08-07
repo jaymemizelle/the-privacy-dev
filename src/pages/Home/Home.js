c76b2c27a5c9ee0a4c9a9dba827b6cdc322b6168
Jayme <jlm_developer@protonmail.com>
2021-08-07 11:19:29 -0700

    initial commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="Home">
+        <h1 className="title">
+          Privacy is not a Privelage.
+          <br />
+          It is a civil human right.
+        </h1>
+    </div>
+  );
+}
+
+export default Home;
