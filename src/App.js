27384072ca3e53fd0390675f1a56fb1295bf20ad
Jayme <jlm_developer@protonmail.com>
2021-08-14 15:33:35 -0500

    added functioning react-router for navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Home from './pages/Home/Home';
-import './App.css';
+import Home from "./pages/Home/Home";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import About from "./pages/About/About";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <main>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Router>
+    </main>
   );
 }
 
--- src/components/MyNavbar/MyNavbar.js
+++ src/components/MyNavbar/MyNavbar.js
@@ -1,23 +1,32 @@
 import React from "react";
-import  {Navbar} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from '../../images/lock-shield.png';
-import './MyNavbar.css';
+import { Navbar, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../../images/lock-shield.png";
+import "./MyNavbar.css";
+import { Link } from "react-router-dom";
 
 function MyNavbar() {
   return (
     <>
       <Navbar>
-          <Navbar.Brand href="#home">
-            <img
-              src={logo}
-              width="30"
-              height="30"
-              className="align-top logo"
-              alt="The Privacy Dev logo"
-            /> 
-            <span className="brand">The Privacy Dev</span>
-          </Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="30"
+            height="30"
+            className="align-top logo"
+            alt="The Privacy Dev logo"
+          />
+          <span className="brand">The Privacy Dev</span>
+        </Navbar.Brand>
+        <Nav className="me-auto">
+        <Nav.Link className="right">
+          <Link className="link" to="/">Home</Link>
+        </Nav.Link>
+        <Nav.Link className="right">
+          <Link className="link" to="/about">About</Link>
+        </Nav.Link>
+        </Nav>
       </Navbar>
     </>
   );
