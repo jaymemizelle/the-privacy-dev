7573258245646a5ef9d3a5cfe26d3004f33ef684
Jayme <jlm_developer@protonmail.com>
2021-08-15 17:25:56 -0500

    added sections folder inside home page folder to make home page component more readable and manageable

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MyNavbar from "../../components/MyNavbar/MyNavbar";
 import "./Home.css";
-import TorImage from "../../images/tor-project.jfif";
+import TorSection from "./Sections/TorSection/TorSection";
 
 function Home() {
   return (
@@ -14,37 +14,7 @@ function Home() {
           It is a civil human right.
         </h1>
       </div>
-      <section className="torSection">
-        <a className="torTitle" rel="noreferrer" href="https://www.torproject.org/" target="_blank">
-          <h2>The Tor Project: Anonymity Online</h2>
-        </a>
-        <div className="torDiv">
-          <img src={TorImage} alt="Tor Project" />
-          <div>
-            <p>
-              The Tor Project is a 501(c)(3) US nonprofit organization dedicated
-              to developing software that allows it's users to browse privately
-              and freely online. The Tor Network is made of thousands of
-              volunteer-run servers known as Tor relays. These servers allow for
-              your internet traffic to be encrypted three times as it passes
-              over the Tor network. This allows for you, the user, to browse the
-              web completely anonymously. Tor strives to make all it's users
-              look the same, which will make it challening for anyone to track
-              you based on your browser or device information.
-            </p>
-            <p>
-              Interested in turning your node into a Tor relay server? Click{" "}
-              <a className="torServerLink"
-                rel="noreferrer"
-                href="https://community.torproject.org/relay/setup/"
-                target="_blank"
-              >
-                here
-              </a>
-            </p>
-          </div>
-        </div>
-      </section>
+      <TorSection />
     </>
   );
 }
---
+++ src/pages/Home/Sections/DuckDuckGoSection/DuckDuckGoSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './DuckDuckGoSection.css';
+
+function DuckDuckGoSection() {
+    return (
+        <h2>DuckDuckGo Search Engine</h2>
+    )
+}
+
+export default DuckDuckGoSection;
