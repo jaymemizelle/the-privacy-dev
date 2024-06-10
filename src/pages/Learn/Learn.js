3e15460d8aacdcc5e0986850989a7fd93c397554
The Privacy Dev <theprivacydev@protonmail.com>
2024-06-09 19:56:19 -0500

    added second pane and third pane coments to be rendered for desktop

--- src/pages/Learn/Learn.js
+++ src/pages/Learn/Learn.js
@@ -1,6 +1,8 @@
 import React from "react";
 import MyNavbar from "../../components/MyNavbar/MyNavbar";
 import "./Learn.css";
+import SecondPane from "../Home/Sections/SecondPane/SecondPane";
+import ThirdPane from "../Home/Sections/ThirdPane/ThirdPane";
 
 function Learn() {
   return (
@@ -14,6 +16,8 @@ function Learn() {
         linkThreePath="/learn"
       />
       <h2>Learn How to Protect Your Online Identity</h2>
+      <SecondPane />
+      <ThirdPane />
     </div>
   );
 }
